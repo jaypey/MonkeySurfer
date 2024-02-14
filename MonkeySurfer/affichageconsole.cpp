d6602cf8b966eb47a7586017a86eabb13a219527
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2024-02-14 17:02:27 -0500

    Ajout contours + score

--- MonkeySurfer/affichageconsole.cpp
+++ MonkeySurfer/affichageconsole.cpp
@@ -2,7 +2,6 @@
 
 AffichageConsole::AffichageConsole(Jeu *j) : Affichage(j) {
     _lastfrm = std::chrono::steady_clock::now();
-    _output.resize(NB_LIGNES + (NB_LIGNES * NB_COLS)); // Définit la capacité de la std::string en avance
     afficherArrierePlan();
 
     // Positionnement des lianes, centré dans la zone de jeu
@@ -42,32 +41,62 @@ void AffichageConsole::afficher() {
 void AffichageConsole::afficherArrierePlan() {
     for (int y = 0; y < NB_LIGNES; y++)
         for (int x = 0; x < NB_COLS; x++)
-            _img[x][y] = '.';
+            _img[x][y] = ' ';
 }
 
 void AffichageConsole::afficherLianes() {
     for (int i = 0; i < NB_LIANES; i++)
         for (int y = 0; y < NB_LIGNES; y++)
-            _img[_xlianes[i]][y] = '|';
+            _img[_xlianes[i]][y] = 'l';
 }
 
 void AffichageConsole::afficherJoueur() {
     // Test - à faire pour de vrai plus tard
-    static int x = 0;
+    static int x = _xlianes[0];
     static int d = 1;
     _img[x][15] = 'M'; // monkey
     x += d;
-    if (x == NB_COLS - 1)
+    if (x == _xlianes[NB_LIANES - 1])
         d = -1;
-    if (x == 0)
+    if (x == _xlianes[0])
         d = 1;
 }
 
-void AffichageConsole::afficherItems() { /* TODO */ }
+void AffichageConsole::afficherItems() {
+    /* PSEUDO-CODE
+    for (int i = 0; i < _jeu->items().taille(); i++)
+        _img[_jeu->items()[i].x()][_jeu->items()[i].y()];
+    */
+}
 
-void AffichageConsole::afficherObstacles() { /* TODO */ }
+void AffichageConsole::afficherObstacles() {
+    /* PSEUDO-CODE
+    for (int i = 0; i < _jeu->obstacles().taille(); i++)
+        _img[_jeu->obstacles()[i].x()][_jeu->obstacles()[i].y()];
+    */
+}
 
-void AffichageConsole::afficherIU() { /* TODO */ }
+void AffichageConsole::afficherIU() {
+    // Coutour zone de jeu + UI
+    for (int i = 0; i < NB_COLS; i++) {
+        _img[i][0] = '-';
+        _img[i][NB_LIGNES - 3] = '-';
+        _img[i][NB_LIGNES - 1] = '-';
+    }
+    for (int i = 1; i < NB_LIGNES - 1; i++) {
+        _img[0][i] = '|';
+        _img[NB_COLS - 1][i] = '|';
+    }
+
+    // Vider l'espace pour afficher le texte clairement
+    for (int i = 1; i < NB_COLS - 1; i++)
+        _img[i][NB_LIGNES - 2] = ' ';
+
+    // Afficher le texte pour le score
+    _score = "Score : " + std::to_string(28 /*_jeu->getScore()*/);
+    for (int i = 0; i < _score.size(); i++)
+        _img[i + 2][NB_LIGNES - 2] = _score[i];
+}
 
 bool AffichageConsole::peutAfficherProchaineImage() {
     auto now = std::chrono::steady_clock::now();
--- MonkeySurfer/affichageconsole.h
+++ MonkeySurfer/affichageconsole.h
@@ -1,15 +1,17 @@
 #ifndef AFFICHAGECONSOLE_H
 #define AFFICHAGECONSOLE_H
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
+#include <string>
 #include "affichage.h"
 
 #define NB_COLS 41
-#define NB_LIGNES 20
+#define NB_LIGNES 25
+
 #define NB_LIANES 5
 #define ECART_LIANES 6
-#define FPS 30 // Images par seconde (Frames per second)
+#define FPS 60 // Images par seconde (Frames per second)
 
 class AffichageConsole : public Affichage
 {
@@ -27,12 +29,18 @@ public:
 
     bool peutAfficherProchaineImage(); // Attendre que l'image précédente aie finie de s'afficher, vitesse d'affichage limitée par le FPS
 private:
+    // Affichage
     char _img[NB_COLS][NB_LIGNES]; // Informations de l'image à imprimer à la console
     std::string _output; // Image à imprimer à la console
     std::chrono::steady_clock::time_point _lastfrm; // Temps écoulé depuis dernier affichage
-    int _xlianes[NB_LIANES]; // Emplacement colonne des lianes
 
     const long long _DURFRM = 1000 / FPS; // Durée d'affichage d'une image avant d'afficher la prochaine, en millisecondes
+
+    // Lianes
+    int _xlianes[NB_LIANES]; // Emplacement colonne des lianes
+
+    // UI
+    std::string _score; // Texte pour afficher le score
 };
 
 #endif // !AFFICHAGECONSOLE_H
