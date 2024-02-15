e517c96b96a6ef35d629c1f0d20adb0634402afd
Mj-gagne <mj.gagne@outlook.com>
2024-02-15 13:54:10 -0500

    Update joueur.h

--- MonkeySurfer/joueur.h
+++ MonkeySurfer/joueur.h
@@ -7,8 +7,20 @@ public:
 	Joueur();
 	~Joueur();
 
+	int getPiece();
+	int getScore();
+
+	void ramasserPiece();
+	void conteurPointage();
+	void ajouterInventaire();
+	void echangerInventaire();
+	void useObjet();
+	
 private:
 
+	int pieces;
+	int Score;
+	int nbObjets;
 };
 
 #endif // !JOUEUR_H
