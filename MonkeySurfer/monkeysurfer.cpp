2dfcac2d1464eb2fa9785029362dd9fdfa1cf41b
gabrielber2222 <gberthiaume@outlook.com>
2024-02-15 14:24:50 -0500

    randomizer terminé

--- MonkeySurfer/monkeysurfer.cpp
+++ MonkeySurfer/monkeysurfer.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "aleatoire.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    RandomGenerator randomnbr;
+    for (int i=0; i<100; i++)
+    {
+        std::cout << randomnbr.random(10, 45, 1545652657) << std::endl;
+    }
+
+
 }
