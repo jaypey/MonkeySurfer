840d4e3a23d91e4a0df452eddf215f1686346dc0
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2024-02-22 15:00:12 -0500

    Librairies json et asio

--- manette_monkeysurfer/src/main.cpp
+++ manette_monkeysurfer/src/main.cpp
@@ -1,4 +1,8 @@
+#include <ArduinoJson.h>
+#include "json.hpp"
 #include "accelerometre.cpp"
+
+JsonDocument doc;
 
 // put function declarations here:
 int myFunction(int, int);
@@ -9,17 +13,20 @@ void setup() {
 }
 
 void loop() {
+    StaticJsonDocument<64> doc;
     // Créer une instance de la classe Accelerometre
     Accelerometre pololo;
 
     // Appeler la fonction shake avec l'argument 'x'
     int resultat = pololo.shake('z');
+    doc["shake"] = resultat;
+
+    // Sérialisez l'objet JSON
+    String jsonStr;
+    serializeJson(doc, jsonStr);
 
     // Faire autre chose avec le résultat si nécessaire
     Serial.println(resultat);
-    //pololo.detection('z');
-    //Serial.println(pololo.detection('z'));
-    
 
     // Attendre un peu avant de recommencer
     delay(200);
