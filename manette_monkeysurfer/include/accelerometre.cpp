beab40e00ecb615a2dd291e656f9c8a495ce8123
gabrielber2222 <gberthiaume@outlook.com>
2024-02-22 10:13:25 -0500

    classes accelerometre finies
    tout fonctionne, calibration à ajuster au besoin

---
+++ manette_monkeysurfer/include/accelerometre.cpp
@@ -0,0 +1,97 @@
+#include "accelerometre.h"
+
+/*Retourne une valeur numérique concernant l'effet de la gravité sur l'axe entré en paramètre. Une valeur positive indique que
+la manette se déplace dans la direction de l'axe tandis qu'une valeur négative dans la direction opposée. Plus la valeur est élevée, 
+plus le déplacement est important*/
+int Accelerometre::detection(char axe)
+{
+    int xMin = 267;
+    int xMax = 400;
+    int yMin = 264;
+    int yMax = 400;
+    int zMin = 275;
+    int zMax = 403;
+    const int samples = 10;
+
+    if ((axe == 'X') || (axe == 'x'))
+    {
+        int xRaw=0;
+        for(int i=0;i<samples;i++)
+        {
+            xRaw+=analogRead(xPin);
+        }
+        xRaw/=samples;
+
+        int xMilliG = map(xRaw, xMin, xMax, -1000, 1000);
+
+        return (xMilliG);
+    }
+
+    if ((axe == 'Y') || (axe == 'y'))
+    {
+       int yRaw=0;
+        for(int i=0;i<samples;i++)
+        {
+            yRaw+=analogRead(yPin);
+        }
+        yRaw/=samples;
+
+        int yMilliG = map(yRaw, yMin, yMax, -1000, 1000);
+
+        return (yMilliG);
+    }
+
+    if ((axe == 'Z') || (axe == 'z'))
+    {
+        int zRaw=0;
+        for(int i=0;i<samples;i++)
+        {
+            zRaw+=analogRead(zPin);
+        }
+        zRaw/=samples;
+
+        int zMilliG = map(zRaw, zMin, zMax, -1000, 1000);
+  
+        return (zMilliG);
+    }
+}
+
+//Vérifie si un secouement sur l'axe entré en paramètre a lieu. Retourne true si secouement, false sinon.
+bool Accelerometre::shake(char axe)
+{
+    if ((axe == 'X') || (axe == 'x'))
+    {
+        if ((detection('x') > 300 )|| (detection('x') < -400))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+     if ((axe == 'Y') || (axe == 'y'))
+    {
+        
+        if ((detection('y') > 200)|| (detection('y') < 200))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+     if ((axe == 'Z') || (axe == 'z'))
+    {
+        
+        if ((detection('z') > 1700)|| (detection('z') < 500))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- manette_monkeysurfer/src/main.cpp
+++ manette_monkeysurfer/src/main.cpp
@@ -1,15 +1,28 @@
-#include <Arduino.h>
+#include "accelerometre.cpp"
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Créer une instance de la classe Accelerometre
+    Accelerometre pololo;
+
+    // Appeler la fonction shake avec l'argument 'x'
+    int resultat = pololo.shake('z');
+
+    // Faire autre chose avec le résultat si nécessaire
+    Serial.println(resultat);
+    //pololo.detection('z');
+    //Serial.println(pololo.detection('z'));
+    
+
+    // Attendre un peu avant de recommencer
+    delay(200);
 }
 
 // put function definitions here:
