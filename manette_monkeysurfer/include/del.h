89abb3a151d7221f3ba44e3c7e859a6f7c8ff2fb
fredbouchard777 <frederique@propage.ca>
2024-02-27 10:10:42 -0500

    del, bargraph et lcd

---
+++ manette_monkeysurfer/include/del.h
@@ -0,0 +1,29 @@
+#ifndef DEL_H
+#define DEL_H
+
+#include <Arduino.h>
+
+class del
+{
+    public:
+        del();
+        del (int sapin);
+        ~del();
+        void setpin(int sapin);
+        int getpin();
+        void instancier();
+        void allumer();
+        void eteindre();
+        void clignoterrapidement();
+        void clignoter();
+        void clignoterlentement();
+
+    private:
+        int pin;
+
+};
+
+#endif
+
+//rouge 23, jaune 25, vert 27, rouge1 22, rouge2 24, rouge3 26, jaune1 28
+//jaune2 30, jaune3 32, jaune4 34, vert1 36, vert2 38, vert3 40
--- manette_monkeysurfer/src/main.cpp
+++ manette_monkeysurfer/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include "del.h"
+#include "del.cpp"
+#include "bargraph.h"
+#include "bargraph.cpp"
+#include <LiquidCrystal.h>
+//rouge 23, jaune 25, vert 27, rouge1 22, rouge2 24, rouge3 26, jaune1 28, jaune2 30, jaune3 32, jaune4 34, vert1 36, vert2 38, vert3 40
 
-// put function declarations here:
-int myFunction(int, int);
+del rouge(23);
+del jaune(25);
+del vert(27);
+bargraph lebargraph;
+LiquidCrystal ecran(39, 37, 35, 33, 31, 29);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  rouge.instancier();
+  jaune.instancier();
+  vert.instancier();
+  lebargraph.instancier(22, 24, 26, 28, 30, 32, 34, 36, 38, 40);
+  ecran.begin(16, 2);
+  ecran.print("Vous etes mort!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() 
+{
+  lebargraph.joie();
+  rouge.allumer();
+  jaune.allumer();
+  vert.allumer();
 }
