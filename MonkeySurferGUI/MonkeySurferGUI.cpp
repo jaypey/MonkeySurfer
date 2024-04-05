5b8b4126e67c98688c84fb95d42de16960208fe8
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2024-04-05 09:49:36 -0400

    Code base scene/view

--- MonkeySurferGUI/MonkeySurferGUI.cpp
+++ MonkeySurferGUI/MonkeySurferGUI.cpp
@@ -1,9 +1,26 @@
-
-#include <iostream>
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsRectItem>
 
-int main()
+int main(int argv, char** args)
 {
-    std::cout << "Hello World!\n";
-}
+	QApplication app(argv, args);
 
+	QGraphicsScene* scene = new QGraphicsScene;
+	scene->setSceneRect(0, 0, 800, 600);
+
+	QGraphicsView* view = new QGraphicsView;
+	view->setScene(scene);
+	view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	view->setFixedSize(800, 600);
+
+	// Singe
+	QGraphicsRectItem* player = new QGraphicsRectItem;
+	player->setRect(350, 250, 100, 100);
+	scene->addItem(player);
+
+	view->show();
+	app.exec();
+}
