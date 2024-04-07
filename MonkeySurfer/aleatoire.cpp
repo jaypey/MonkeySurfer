890d0c5f103fffe3e50f0646e36d089c09084f0a
gabrielber2222 <gberthiaume@outlook.com>
2024-04-07 15:16:02 -0400

    Update fonction random
    Update de la classe aléatoire pour générer une valeur aléatoire distincte même si aucun nouveau muon n'est détecté par la Cosmic Watch

--- MonkeySurfer/aleatoire.cpp
+++ MonkeySurfer/aleatoire.cpp
@@ -2,6 +2,19 @@
 #include <string>
 #include "aleatoire.h"
 
+/*Si aucun nouveau muon n'est détecté (la variable "muons" n'est ainsi pas incrémenté), on crée une valeur semi-aléatoire 
+de muons permettant un random plus uniforme (empêche que la fonction random stagne et génère la même valeur)*/
+void RandomGenerator::verificateurNouveauMuon(int muons, int derniermuons)
+{
+    if (muons == derniermuons)
+    {
+        muons = (muons + muons) * (((muons) % 13) + 1);
+    }
+}
+
+/*Génère un nombre random compris inclusivement entre la borne inférieure et supérieure selon la valeur de "muons" 
+("muons" s'incrémente avec le code Arduino relié au Cosmic Watch mais aussi avec la fonction verificateurNouveauMuon 
+si aucune nouveau muon n'est détecté)*/
 int RandomGenerator::random(int borneinf, int bornesup, int muons)
 {
     int nbr;
